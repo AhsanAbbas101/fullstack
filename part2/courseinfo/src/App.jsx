b62fc8264a008e5fdc5eb525409b23d7122975b2
Ahsan Abbas <ahsanabbas468@gmail.com>
2024-03-24 01:32:11 +0200

    part2 exercise 2.5 commit
    Exercise 2.5 completion commit

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,29 +1,5 @@
 
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
-}
-
-const Header = ({ course }) => <h1>{course}</h1>
-
-const Total = ({ parts }) => {
-  const total =  parts.reduce((s,p) => s+p.exercises,0)
-  return <p><b>total of {total} exercises</b></p>
-}
-
-const Part = ({ part }) => 
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
-const Content = ({ parts }) => 
-  parts.map(part => <Part key={part.id} part={part}/>)
-
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
