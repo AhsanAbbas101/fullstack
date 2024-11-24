16f79cc27ed298baa6acc07e29a252235e3bde9f
Ahsan Abbas <ahsanabbas468@gmail.com>
2024-11-24 23:13:01 +0200

    part7 exercises 7.4-7.8 commit

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -10,6 +10,8 @@ import {
 } from 'react-router-dom'
 
 
+import { useField } from './hooks'
+
 const Menu = ({ notification }) => {
   const padding = {
     paddingRight: 5
@@ -66,38 +68,49 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('text')//useState('')
+  const author = useField('text')//useState('')
+  const info = useField('text')//useState('')
 
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.state.value,
+      author: author.state.value,
+      info: info.state.value,
       votes: 0
     })
+  }
+
+  const handleReset = (e) => {
+    e.preventDefault()
+    content.reset()
+    author.reset()
+    info.reset()
   }
 
   return (
     <div>
       <h2>create a new anecdote</h2>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} onReset={handleReset} >
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input name='content' {...content.state} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input name='author' {...author.state}  />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input name='info' {...info.state} />
         </div>
-        <button>create</button>
+        <span>
+        <input type='submit' value='create'/>
+        <input type='reset' value='reset'/>
+        </span>
+
       </form>
     </div>
   )
